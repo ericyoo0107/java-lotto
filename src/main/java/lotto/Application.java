5705fb9b2d33b8df7b3fda764f6963f2d016110c
YuJunHyuk <ericyoo0107@gmail.com>
2022-11-11 19:03:39 +0900

    <feat> 입출력 인터페이스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,33 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int userMoney = Integer.parseInt(Console.readLine());
+        User JunHyuk = new User(userMoney);
+        JunHyuk.buyLotto();
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String lottoNumString = Console.readLine();
+        String[] lottoNumList = lottoNumString.split(",");
+        List<Integer> lottoNumInt = new ArrayList<>();
+        for (String s : lottoNumList) {
+            lottoNumInt.add(Integer.parseInt(s));
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNum = Console.readLine();
+        LottoMachine lottoMachine = new LottoMachine(lottoNumInt, Integer.parseInt(bonusNum));
+
+        lottoMachine.makeLottoResult(JunHyuk.getMyLotto());
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        lottoMachine.showResult();
+        double yield = lottoMachine.getReturnMoney() / userMoney;
+        System.out.println("총 수익률은 " + yield + "%입니다.");
     }
 }
