5fbd23b33d472fbad2ae297cec2dd77c89b8389e
YuJunHyuk <ericyoo0107@gmail.com>
2022-11-11 16:15:22 +0900

    <feat> user 클래스 readme 대로 개발

---
+++ src/main/java/lotto/User.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class User {
+	private final List<Lotto> myLotto = new ArrayList<>();
+	int money;
+
+	public User(int money)
+	{
+		moneyCheck(money);
+		this.money=money;
+	}
+	public int moneyCheck (int money)
+	{
+		if(money % 1000 != 0)
+			throw new IllegalArgumentException();
+		return money;
+	}
+	public void buyLotto()
+	{
+		for(int i=money; i>0; i-=1000)
+		{
+			List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+			Lotto lotto = new Lotto(numbers);
+			myLotto.add(lotto);
+		}
+	}
+}
