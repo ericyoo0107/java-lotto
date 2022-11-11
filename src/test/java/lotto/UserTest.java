244c620c4304ac2a36e1540ab345b6abafde117c
YuJunHyuk <ericyoo0107@gmail.com>
2022-11-11 16:40:14 +0900

    <test> user 클래스 테스트 추가

---
+++ src/test/java/lotto/UserTest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+	@Test
+	void 천원_단위로_안들어오면_exception()
+	{
+		assertThatThrownBy(() -> new User(2100))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+	@Test
+	void 팔천원으로_로또8개_사기()
+	{
+		User user = new User(8000);
+		user.buyLotto();
+		int size = user.getMyLotto().size();
+		assertEquals(size, 8);
+
+	}
+}
