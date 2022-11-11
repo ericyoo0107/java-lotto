88c5e438ab0f437bac4f7975563e514a42dc8bb4
YuJunHyuk <ericyoo0107@gmail.com>
2022-11-11 16:51:12 +0900

    <feat> lotto 클래스에 lotto 숫자가 잘못 들어올 경우 exception 발생하도록 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,14 +7,21 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numberRange1_45(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers) { //숫자가 6개인지 체크
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void numberRange1_45(List<Integer> numbers) {
+        for (Integer num : numbers) {
+            if (num < 1 || num > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
